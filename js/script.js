a66f2e51c42b2bdc0f603d322276e24bd3a9a1e5
Asan Azimkulov <azimkulov40@gmail.com>
2022-05-27 14:08:43 +0600

    added slider and calculator

--- js/script.js
+++ js/script.js
@@ -80,5 +80,202 @@ window.addEventListener("DOMContentLoaded", function () {
 
     setClock("timer", deadline);
 
+
+    // Modal
+    const openModalBtn = document.querySelector(".more")
+    const modal = document.querySelector(".overlay")
+    const closeModalBtn = document.querySelector(".popup-close")
+    function openModal() {
+        modal.style.display = "block";
+        openModalBtn.classList.add("more-splash")
+        document.body.style.overflow = "hidden"
+        closeModalBtn.addEventListener("click", closeModal)
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+        openModalBtn.classList.remove("more-splash")
+        document.body.style.overflow = ""
+        closeModalBtn.removeEventListener("click", closeModal)
+
+    }
+    openModalBtn.addEventListener("click", openModal)
+
+    // modal for Tabs
+    const tabsFrame = document.querySelector(".info")
+    tabsFrame.addEventListener("click", function (evt) {
+        if (evt.target && evt.target.className === "description-btn") {
+            openModal()
+        }
+    })
+
+    // Requests
+    const contactForm = document.querySelector(".main-form")
+    const contactFormPhone = contactForm.querySelector(".popup-form__input")
+    const mainForm = document.querySelector("#form")
+    const mainFormMail = mainForm.querySelector("input[type='email']")
+    const mainFormPhone = mainForm.querySelector("input[type='tel']")
+    contactForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+
+        const data = {
+            phone: contactFormPhone.value
+        }
+        contactFormPhone.value = ""
+        closeModal()
+        const request = new XMLHttpRequest();
+        request.open('POST', "server.php", true);
+
+        request.setRequestHeader('Content-type', 'application/json');
+
+        request.onreadystatechange = function () {//Call a function when the state changes.
+            if (request.readyState == 4 && request.status == 200) {
+                alert(request.responseText);
+            }
+        }
+        request.send(JSON.stringify(data));
+    })
+    mainForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        const data = {
+            mail: mainFormMail.value,
+            phone: mainFormPhone.value
+        }
+        mainFormMail.value = ""
+        mainFormPhone.value = ""
+        const request = new XMLHttpRequest();
+
+        request.open('POST', "server.php", true);
+
+        request.setRequestHeader("POST", "server.php", true);
+
+        request.onreadystatechange = function () {//Call a function when the state changes.
+            if (request.readyState == 4 && request.status == 200) {
+                alert(request.responseText);
+            }
+        }
+
+        request.send(JSON.stringify(data));
+
+    })
+
+    // Slider
+    let slideIndex = 1;
+    const slides = document.querySelectorAll(".slider-item"),
+        prev = document.querySelector(".prev"),
+        next = document.querySelector(".next"),
+        dotsWrap = document.querySelector(".slider-dots"),
+        dots = document.querySelectorAll(".dot");
+
+    setVisibleSlide(slideIndex)
+
+
+    function setVisibleSlide(slide) {
+
+        if (slide > slides.length) {
+            slideIndex = 1;
+        }
+        if (slide < 1) {
+            slideIndex = slides.length
+        }
+        slides.forEach((item) => {
+
+            item.style.display = "none";
+        })
+        dots.forEach((item) => {
+            item.classList.remove("dot-active")
+        })
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].classList.add("dot-active")
+    }
+    function plusSlides(n) {
+        setVisibleSlide(slideIndex += n)
+    }
+    function currentSlide(n) {
+        setVisibleSlide(slideIndex = n)
+    }
+    prev.addEventListener("click", () => {
+        plusSlides(-1)
+    });
+    next.addEventListener("click", () => {
+        plusSlides(1)
+    });
+    dotsWrap.addEventListener("click", (evt) => {
+        for (let i = 0; i < dots.length; i++) {
+            if (evt.target.classList.contains("dot") && evt.target === dots[i]) {
+                currentSlide(i + 1)
+            }
+        }
+
+    })
+
+    // Calculator
+
+    const persons = document.querySelectorAll(".counter-block-input")[0],
+        restDays = document.querySelectorAll(".counter-block-input")[1],
+        place = document.getElementById("select"),
+        totalValue = document.getElementById("total");
+
+    let personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.textContent = 0;
+    persons.addEventListener("change", function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000
+        if (restDays.values === "") {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    })
+    persons.addEventListener("change", function () {
+        personsSum = +this.value;
+        if (!personsSum) {
+            totalValue.textContent = 0;
+            return;
+        }
+        total = (daysSum + personsSum) * 4000
+        if (restDays.value === "") {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    })
+    restDays.addEventListener("change", function () {
+        daysSum = +this.value;
+        if (!daysSum) {
+            totalValue.textContent = 0;
+            return;
+        }
+        total = (daysSum + personsSum) * 4000
+        if (persons.value === "") {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    })
+
+    place.addEventListener("change", function () {
+        if (restDays.value === "" || persons.value === "") {
+            totalValue.textContent = 0;
+        } else {
+            let a = total;
+            totalValue.textContent = a * this.options[this.selectedIndex].value
+        }
+    })
+
+
+
+
+
+
+
+
+
+
 })
 
